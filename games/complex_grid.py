e3b88f75802cb1bf1b2da72ce76cd8963e16222e
valkryhx <283306449@qq.com>
2024-02-26 21:20:58 +0800

    info

--- games/complex_grid.py
+++ games/complex_grid.py
@@ -102,8 +102,8 @@ class MuZeroConfig:
 
         ### Replay Buffer
         self.replay_buffer_size = 5000  # Number of self-play games to keep in the replay buffer
-        self.num_unroll_steps = 7  # Number of game moves to keep for every batch element
-        self.td_steps = 7  # Number of steps in the future to take into account for calculating the target value
+        self.num_unroll_steps = 5  # Number of game moves to keep for every batch element
+        self.td_steps = 5  # Number of steps in the future to take into account for calculating the target value
         self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
         self.PER_alpha = 0.5  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
 
