df847a6e254406e0214fe5a4488eb95dd9c2475b
valkryhx <283306449@qq.com>
2024-02-27 22:33:51 +0800

    Update complex_grid.py

--- games/complex_grid.py
+++ games/complex_grid.py
@@ -44,7 +44,7 @@ class MuZeroConfig:
         
         self.selfplay_on_gpu = False#True #False
         self.max_moves = grid_size//2#6  # Maximum number of moves if game is not finished before
-        self.num_simulations = 20 # Number of future moves self-simulated
+        self.num_simulations = 100 # Number of future moves self-simulated
         self.discount = 1# 0.978  # Chronological discount of the reward
         self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
 
@@ -66,9 +66,9 @@ class MuZeroConfig:
         self.downsample = False  # Downsample observations before representation network, False / "CNN" (lighter) / "resnet" (See paper appendix Network Architecture)
         self.blocks = 6#1  # Number of blocks in the ResNet
         self.channels = 128#2  # Number of channels in the ResNet
-        self.reduced_channels_reward = 2#2  # Number of channels in reward head
-        self.reduced_channels_value = 2#2  # Number of channels in value head
-        self.reduced_channels_policy = 4#2  # Number of channels in policy head
+        self.reduced_channels_reward = 8#2  # Number of channels in reward head
+        self.reduced_channels_value = 8#2  # Number of channels in value head
+        self.reduced_channels_policy = 16#2  # Number of channels in policy head
         self.resnet_fc_reward_layers = [64]  # Define the hidden layers in the reward head of the dynamic network
         self.resnet_fc_value_layers = [64]  # Define the hidden layers in the value head of the prediction network
         self.resnet_fc_policy_layers = [64]  # Define the hidden layers in the policy head of the prediction network
@@ -86,7 +86,7 @@ class MuZeroConfig:
         ### Training
         self.results_path = pathlib.Path(__file__).resolve().parents[1] / "results" / pathlib.Path(__file__).stem / datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")  # Path to store the model weights and TensorBoard logs
         self.save_model = True  # Save the checkpoint in results_path as model.checkpoint
-        self.training_steps = 200000#30000  # Total number of training steps (ie weights update according to a batch)
+        self.training_steps = 400000#30000  # Total number of training steps (ie weights update according to a batch)
         self.batch_size =  32  # Number of parts of games to train on at each training step
         self.checkpoint_interval = 10#10  # Number of training steps before using the model for self-playing
         self.value_loss_weight = 0.25#0.25  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
