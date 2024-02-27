50aec0f7e5ceef246614b3e8c63617e1e8f7ddf3
valkryhx <283306449@qq.com>
2024-02-27 14:55:06 +0800

    info

--- games/complex_grid.py
+++ games/complex_grid.py
@@ -323,9 +323,9 @@ class GridEnv:
         #done =  len(self.legal_actions())==0
         if done :
             if self.agent_get_reward>=self.h_score :
-                reward += 5
+                reward += self.agent_get_reward-self.h_score#5
             else:
-                reward -= 5
+                reward -= self.agent_get_reward-self.h_score#5
         
         return self.get_observation(), reward, done#bool(reward)
 
