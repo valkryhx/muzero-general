e4303c668a8a7b1c11cfe79fa80f403cf8c0489e
valkryhx <283306449@qq.com>
2024-02-28 14:40:49 +0800

    info

--- games/complex_grid.py
+++ games/complex_grid.py
@@ -27,10 +27,10 @@ class MuZeroConfig:
         #self.observation_shape = (1, 1, grid_size*grid_size)
         #self.observation_shape = (1,grid_size, grid_size)
         # grid和marked_position 两个np.array 所以是2 。这次不在grid上修改保留原始信息
-        self.observation_shape = (2,grid_size, grid_size)
+        self.observation_shape = (1,grid_size, grid_size)
         self.action_space = list(range(grid_size*grid_size))#list(range(2))  # Fixed list of all possible actions. You should only edit the length
         self.players = list(range(1))  # List of players. You should only edit the length
-        self.stacked_observations = 3  # Number of previous observations and previous actions to add to the current observation
+        self.stacked_observations = 0  # Number of previous observations and previous actions to add to the current observation
 
         # Evaluate
         self.muzero_player = 0  # Turn Muzero begins to play (0: MuZero plays first, 1: MuZero plays second)
@@ -46,7 +46,7 @@ class MuZeroConfig:
         
         self.selfplay_on_gpu = False#True #False
         self.max_moves = grid_size//2#6  # Maximum number of moves if game is not finished before
-        self.num_simulations = 400 # Number of future moves self-simulated
+        self.num_simulations = 50 # Number of future moves self-simulated
         self.discount = 1# 0.978  # Chronological discount of the reward
         self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
 
@@ -66,7 +66,7 @@ class MuZeroConfig:
         
         # Residual Network
         self.downsample = False  # Downsample observations before representation network, False / "CNN" (lighter) / "resnet" (See paper appendix Network Architecture)
-        self.blocks = 6#1  # Number of blocks in the ResNet
+        self.blocks = 16#1  # Number of blocks in the ResNet
         self.channels = 128#2  # Number of channels in the ResNet
         self.reduced_channels_reward = 2#2  # Number of channels in reward head
         self.reduced_channels_value = 2#2  # Number of channels in value head
@@ -89,7 +89,7 @@ class MuZeroConfig:
         self.results_path = pathlib.Path(__file__).resolve().parents[1] / "results" / pathlib.Path(__file__).stem / datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")  # Path to store the model weights and TensorBoard logs
         self.save_model = True  # Save the checkpoint in results_path as model.checkpoint
         self.training_steps = 20000#30000  # Total number of training steps (ie weights update according to a batch)
-        self.batch_size =  512  # Number of parts of games to train on at each training step
+        self.batch_size =  16  # Number of parts of games to train on at each training step
         self.checkpoint_interval = 50#10  # Number of training steps before using the model for self-playing
         self.value_loss_weight = 1#0.25  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
         self.train_on_gpu = torch.cuda.is_available()  # Train on GPU if available
@@ -325,16 +325,16 @@ class GridEnv:
         #不能写成reward = self.grid[self.position] 因为self.position=[1,1] 会导致grid[1,1]取得是两行
         # 或者写成reward = self.grid[self.position[0],self.position[1]] 
         #reward = self.grid[*self.position] 
-        reward = self.grid[self.position[0],self.position[1]]  - self.mark[self.position[0],self.position[1]] #- self.h_score / (grid_size/2)
+        reward = self.grid[self.position[0],self.position[1]]  #- self.mark[self.position[0],self.position[1]] #- self.h_score / (grid_size/2)
         self.agent_get_reward += reward
         #print(f'123reward={reward}')
         # grid 变化太剧烈? 所以换成mark来记录已经不能下的位置
-        #self.grid[self.position, :] = self.MARK_NEGATIVE
-        #self.grid[:, self.position] = self.MARK_NEGATIVE
+        self.grid[self.position, :] = self.MARK_NEGATIVE
+        self.grid[:, self.position] = self.MARK_NEGATIVE
         self.mark[self.position, :] = self.MARK_NEGATIVE
         self.mark[:, self.position] = self.MARK_NEGATIVE
-        #done = (numpy.max(self.grid) <= self.MARK_NEGATIVE) or len(self.legal_actions())==0
-        done = (numpy.max(self.mark) <= self.MARK_NEGATIVE) or len(self.legal_actions())==0
+        done = (numpy.max(self.grid) <= self.MARK_NEGATIVE) or len(self.legal_actions())==0
+        #done = (numpy.max(self.mark) <= self.MARK_NEGATIVE) or len(self.legal_actions())==0
         #done =  len(self.legal_actions())==0
         #reward =0
         #if done :
@@ -381,8 +381,8 @@ class GridEnv:
     def get_observation(self):
         #observation = numpy.zeros((self.size, self.size))
         #observation[self.position[0]][self.position[1]] = 1
-        observation = [self.grid ,self.mark]
-        #observation = [self.grid]
+        #observation = [self.grid ,self.mark]
+        observation = [self.grid]
         # flatten 把二维3x3 拉成 单独的1维为9的np array
         #return observation.flatten()
         return numpy.array(observation)
